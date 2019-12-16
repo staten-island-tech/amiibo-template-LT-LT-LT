c0a8a0b7352cfcb76229bf4d29efba69b282eb78
LT-LT-LT <57498881+LT-LT-LT@users.noreply.github.com>
2019-12-16 14:18:31 -0500

    Init

--- public/index.js
+++ public/index.js
@@ -3,10 +3,10 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Thami likes bagels and butter. Also he smells");
+  res.send("Gay");
 });
-app.get("/thamisucks", (req, res) => {
-  res.send("Thami sucks. And Yes I have to do this while you eat");
+app.get("/gay", (req, res) => {
+  res.send("Gay but 2");
 });
 app.listen(3000, () => {
   console.log("Listening on port 3000");
