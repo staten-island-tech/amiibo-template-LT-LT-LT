2e1473b71a968aaad0cb946ae1134e2a25128563
LT-LT-LT <57498881+LT-LT-LT@users.noreply.github.com>
2019-12-17 11:44:47 -0500

    1

--- public/index.js
+++ public/index.js
@@ -2,14 +2,23 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.get("", (req, res) => {
-  res.send("Gay");
+const publiDirectoryParh = path.join(__dirname,"../public");
+const viewsPath = path.join(__dirname,"../templates/views");
+
+app.set("views engine","hbs");//telling handlebars/NODE to use handlebars for templates
+app.set("views", viewsPath);//telling express to get templates from templates/views folder
+app.get("", async (req, res) => {
+  try{res.render("index");
+  } catch (error) {
+    res.status(500).send();
+  }
 });
-app.get("/gay", (req, res) => {
-  res.send("Gay but 2");
+app.get("/g", (req, res) => {
+  res.send("G but 2");
 });
 app.listen(3000, () => {
   console.log("Listening on port 3000");
+  console.log(__dirname)
 });
 
 //127.0.0.1:3000
