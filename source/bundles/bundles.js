e2abab672a2755679de837a9972b2ad57517bb95
LT-LT-LT <57498881+LT-LT-LT@users.noreply.github.com>
2020-01-02 11:46:30 -0500

    2

--- source/bundles/bundles.js
+++ source/bundles/bundles.js
@@ -5,4 +5,11 @@ elements.amiiboForm.addEventListener("keypress", function(event){
         event.preventDefault();
         document.querySelector(".btn").click();
     }
-})
+});
+
+function search(){
+    elements.amiiboForm.addEventListener("submit", async function(e){
+        e.preventDefault()
+        const amiiboURL = `https://www.amiiboapi.com/api/amiibo/?character=${elements.amiiboName.value}`;
+    });
+}
